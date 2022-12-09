30329d539cdb338a95dff985ddba09f67f08dfb7
Chinmay Kabi <chinmay@blend.to>
2022-12-10 03:36:52 +0530

    basic chat working

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+final apiKey = "my8m32m792hh";
+final userToken =
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoid2VhdGhlcmVkLXN1bnNldC0wIn0.fXqI_yTYHtf6Cu1w-wLNi3rBYoWp40c7Ds1YFm52_NE";
+
+/// Create a new instance of [StreamChatClient] passing the apikey obtained from
+/// your project dashboard.
+final client = StreamChatClient(
+  'my8m32m792hh',
+  logLevel: Level.INFO,
+);
+final channel = client.channel(
+  'messaging',
+  id: 'flutterdevs',
+  extraData: {
+    'name': 'Flutter devs',
+  },
+);
+
+void main() async {
+  /// Set the current user and connect the websocket.
+  /// In a production scenario, this should be done using a backend to generate
+  /// a user token using our server SDK.
+  /// Please see the following for more information:
+  /// https://getstream.io/chat/docs/ios_user_setup_and_tokens/
+  await client.connectUser(
+    User(id: 'super-band-9'),
+    userToken,
+  );
+  await channel.watch();
+  runApp(MaterialApp(
+    builder: (context, child) {
+      return StreamChat(client: client, child: child);
+    },
+    home: const WebsideApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class WebsideApp extends StatelessWidget {
+  const WebsideApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Webside chat', theme: ThemeData(), home: const Homepage());
-  }
-}
-
-class Homepage extends StatelessWidget {
-  const Homepage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text('Webside chat');
+    return StreamChannel(
+      channel: channel,
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            final message = Message(
+              text:
+                  'I told them I was pesca-pescatarian. Which is one who eats solely fish who eat other fish. ${DateTime.now.toString()}',
+              extraData: {
+                'customField': '123',
+              },
+            );
+            // await channel.sendMessage(message);
+          },
+        ),
+        body: Column(
+          children: [
+            Expanded(child: StreamMessageListView()),
+            StreamMessageInput()
+          ],
+        ),
+      ),
+    );
   }
 }
